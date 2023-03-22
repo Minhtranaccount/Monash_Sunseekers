c42223682a9b5cce735e6ea2cf1563d150e4cb45
Kcollins85 <k.collins@hotmail.com>
2023-03-22 20:11:15 +1100

    upload latest chart files

--- static/js/chartapp.js
+++ static/js/chartapp.js
@@ -1,12 +1,14 @@
-// Load D3 library
+// Load url data
 let url1 = "/api/states";
 
-// Load data using Axios
+// Load data using D3
 d3.json(url1).then(function(data) {
 
   // Parse data into chartData format
   var chartData = data.map(function(state) {
     return {
+      Capacity: state.Capacity,
+      Potential_kilowatts: state.Potential_kilowatts,
       installations: state.Installations,
       dwellings: state.Est_Dwellings,
       state: state.State
@@ -15,9 +17,9 @@ d3.json(url1).then(function(data) {
 
   console.log(chartData);
 
-  // Create chart using D3 and Chart.js
-  var ctx = d3.select("#myChart").node().getContext("2d");
-  var chart = new Chart(ctx, {
+  // creat the bar chart using D3 and chart.js
+  var ctx1 = d3.select("#myChart").node().getContext("2d");
+  var chart1 = new Chart(ctx1, {
     type: "bar",
     data: {
       labels: chartData.map(function(state) {
@@ -41,6 +43,44 @@ d3.json(url1).then(function(data) {
           backgroundColor: "rgba(54, 162, 235, 0.2)",
           borderColor: "rgba(54, 162, 235, 1)",
           borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  // Create the line graph using D3 and Chart.js
+  var ctx2 = d3.select("#myChart2").node().getContext("2d");
+  var chart2 = new Chart(ctx2, {
+    type: "line",
+    data: {
+      labels: chartData.map(function(state) {
+        return state.state;
+      }),
+      datasets: [
+        {
+          label: "Current Capacity (KW)",
+          data: chartData.map(function(state) {
+            return state.Capacity;
+          }),
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          fill: false
+        },
+        {
+          label: "Potential Capacity (KW)",
+          data: chartData.map(function(state) {
+            return state.Potential_kilowatts;
+          }),
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1,
+          fill: false
         }
       ]
     },
