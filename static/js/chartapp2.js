1dd1242c5640b38684b084bada49bdcdf8cd766b
reinierandrew <reinierandrew@me.com>
2023-03-22 17:39:39 +1100

    Create chartapp2.js

---
+++ static/js/chartapp2.js
@@ -0,0 +1,57 @@
+// Load D3 library
+let url1 = "/api/states";
+
+// Load data using Axios
+d3.json(url1).then(function(data) {
+
+  // Parse data into chartData format
+  var chartData = data.map(function(state) {
+    return {
+      installations: state.Installations,
+      dwellings: state.Est_Dwellings,
+      state: state.State
+    };
+  });
+
+  console.log(chartData);
+
+  // Create chart using D3 and Chart.js
+  var ctx = d3.select("#myChart").node().getContext("2d");
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: chartData.map(function(state) {
+        return state.state;
+      }),
+      datasets: [
+        {
+          label: "Installations",
+          data: chartData.map(function(state) {
+            return state.installations;
+          }),
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1
+        },
+        {
+          label: "Est. Dwellings",
+          data: chartData.map(function(state) {
+            return state.dwellings;
+          }),
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}).catch(function(error) {
+  console.error(error);
+});
