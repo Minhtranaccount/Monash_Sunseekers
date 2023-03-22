4ea737fb8a5c41d8f82dd745b81b83f58884e413
reinierandrew <reinierandrew@me.com>
2023-03-23 07:02:28 +1100

    test

--- static/js/chartapp.js
+++ static/js/chartapp.js
@@ -1,66 +1,86 @@
-// Load D3 library
+// Load url data
 let url1 = "/api/states";
 
-// Load data using Axios
+// Load data using D3
 d3.json(url1).then(function(data) {
 
   // Parse data into chartData format
   var chartData = data.map(function(state) {
     return {
-      // installations: state.Installations,
-      // dwellings: state.Est_Dwellings,
-      capacity: state.Capacity,
-      potential: state.Potential_kilowatts,
+      Capacity: state.Capacity,
+      Potential_kilowatts: state.Potential_kilowatts,
+      installations: state.Installations,
+      dwellings: state.Est_Dwellings,
       state: state.State
     };
   });
 
   console.log(chartData);
 
-  // Create chart using D3 and Chart.js
-  var ctx = d3.select("#myChart").node().getContext("2d");
-  var chart = new Chart(ctx, {
+  // creat the bar chart using D3 and chart.js
+  var ctx1 = d3.select("#myChart").node().getContext("2d");
+  var chart1 = new Chart(ctx1, {
     type: "bar",
     data: {
       labels: chartData.map(function(state) {
         return state.state;
       }),
       datasets: [
-        // {
-        //   label: "Installations",
-        //   data: chartData.map(function(state) {
-        //     return state.installations;
-        //   }),
-        //   backgroundColor: "rgba(255, 99, 132, 0.2)",
-        //   borderColor: "rgba(255, 99, 132, 1)",
-        //   borderWidth: 1
-        // },
-        // {
-        //   label: "Est. Dwellings",
-        //   data: chartData.map(function(state) {
-        //     return state.dwellings;
-        //   }),
-        //   backgroundColor: "rgba(54, 162, 235, 0.2)",
-        //   borderColor: "rgba(54, 162, 235, 1)",
-        //   borderWidth: 1
-        // },
         {
-          label: "Capacity",
+          label: "Installations",
           data: chartData.map(function(state) {
-            return state.capacity;
-          }),
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 1
-        },
-        {
-          label: "Potential",
-          data: chartData.map(function(state) {
-            return state.potential;
+            return state.installations;
           }),
           backgroundColor: "rgba(255, 99, 132, 0.2)",
           borderColor: "rgba(255, 99, 132, 1)",
           borderWidth: 1
+        },
+        {
+          label: "Est. Dwellings",
+          data: chartData.map(function(state) {
+            return state.dwellings;
+          }),
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  // Create the line graph using D3 and Chart.js
+  var ctx2 = d3.select("#myChart2").node().getContext("2d");
+  var chart2 = new Chart(ctx2, {
+    type: "line",
+    data: {
+      labels: chartData.map(function(state) {
+        return state.state;
+      }),
+      datasets: [
+        {
+          label: "Current Capacity (KW)",
+          data: chartData.map(function(state) {
+            return state.Capacity;
+          }),
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          fill: false
+        },
+        {
+          label: "Potential Capacity (KW)",
+          data: chartData.map(function(state) {
+            return state.Potential_kilowatts;
+          }),
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1,
+          fill: false
         }
       ]
     },
